5ba84b9f2284ba707cc18da72ae7249f938022ac
Trevor <trevswizle@gmail.com>
2024-06-13 14:38:10 -0600

    updated

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("This is a test of what to do...");
+        Console.WriteLine("I got my C# Running - yah.");
+
+        // int x = 10;
+        // x = 12;
+        // double y = 10.123;
+        // float z = 10.4F;
+        // string a = "blb";
+        string myName = "trevor";
+
+        Console.WriteLine(myName);
     }
 }
