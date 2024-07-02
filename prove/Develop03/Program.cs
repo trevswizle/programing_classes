c33324c9a76165ae6f49f659380642d43bcf67be
Trevor <trevswizle@gmail.com>
2024-07-01 19:56:43 -0600

    What i can

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,25 +1,66 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
+    static void Main(string[] args)
+    {
+        // Example usage
+        Reference reference = new Reference("John", 3, new List<int> { 16 });
+        Scripture scripture = new Scripture(new List<string> { "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life." });
+        
+        Console.WriteLine(reference.GetReference());
+        Console.WriteLine(scripture.GetScripture());
 
+        Word word = new Word();
+        word.ParseScripture(scripture);
+    }
 }
+
 class Scripture
-// words <list>: string
-// get script
 {
+    private List<string> words;
 
+    public Scripture(List<string> words)
+    {
+        this.words = words;
+    }
+
+    public string GetScripture()
+    {
+        return string.Join(" ", words);
+    }
 }
-class Refrence
-// book: string
-// chapter: int
-// verse <list>: int
+
+class Reference
 {
+    private string book;
+    private int chapter;
+    private List<int> verses;
 
+    public Reference(string book, int chapter, List<int> verses)
+    {
+        this.book = book;
+        this.chapter = chapter;
+        this.verses = verses;
+    }
+
+    public string GetReference()
+    {
+        return $"{book} {chapter}:{string.Join(",", verses)}";
+    }
 }
+
 class Word
-// words: string
-// parsescript(verse);
 {
+    public void ParseScripture(Scripture scripture)
+    {
+        string script = scripture.GetScripture();
+        string[] words = script.Split(' ');
 
+        foreach (var word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
 }
--- scriptures.py
+++ scriptures.py
@@ -1,16 +1,33 @@
 # python
 
 def main():
-    scriture_dict = {}
+    scriture_dict = {"Nephi 1:1": "I Nephi", "Genisis 1:1": "In the Beggining"}
 
-    while True:
-        sriputre_title = input("What verse or verse do you want to learn: ")
-        if sriputre_title == "exit":
-            break
-        scripture = input("What is the scripture: ")
-        scriture_dict[sriputre_title] = scripture
+    # while True:
+    #     print(scriture_dict)
+    #     sriputre_title = input("What verse or verse do you want to learn: ")
+    #     if sriputre_title == "exit":
+    #         break
+    #     scripture = input("What is the scripture: ")
+    #     scriture_dict[sriputre_title] = scripture
     
-    print("Created Dictionarey:")
-    print(scriture_dict)
+    genisis_vers_one = scriture_dict["Genisis 1:1"]
+    nephi_one_one = scriture_dict["Nephi 1:1"]
+    while True:
+        print("Nephi 1:1")
+        print("Genisis 1:1")
+        user_input = input("What verse do you want to see: ")
+        if user_input == "Nephi 1:1":
+            print(nephi_one_one)
+        elif user_input == "Genisis 1:1":
+            print(genisis_vers_one)
+        elif user_input == "exit":
+            break
+        else:
+            print("ERROR: try again")
+
+
+    # print("Created Dictionarey:")
+    # print(scriture_dict)
 
 main()
