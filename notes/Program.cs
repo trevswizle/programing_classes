a82565fb1a0f293ade94fc2fbeb5faa7add24e54
Trevor <trevswizle@gmail.com>
2024-07-08 14:11:32 -0600

    fixed

--- notes/Program.cs
+++ notes/Program.cs
@@ -1,9 +1,9 @@
 ﻿using system;
 
-class program
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("Hello World!");
     }
 }
