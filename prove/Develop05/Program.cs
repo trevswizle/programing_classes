979497e4204aa483d4a4c1364afd35e7b5e2455c
Trevor <trevswizle@gmail.com>
2024-07-11 14:26:29 -0600

    develop05 menu done

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,12 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello world");
+        DisplayMenu menu = new DisplayMenu();
+        menu.DisplayMainMenu();
     }
 }
-class Menu
+class DisplayMenu
 {
+        public void DisplayMainMenu()
+        {
+            bool running = true;
+            while (running)
+            {
+        
+                Console.WriteLine("1. Create a Goal");
+                Console.WriteLine("2. List Goals");
+                Console.WriteLine("3. Save Goals");
+                Console.WriteLine("4. Load Goals");
+                Console.WriteLine("5. Record Event");
+                Console.WriteLine("6. Quit");
 
+                string user_input = Console.ReadLine();
+
+                if (user_input == "1")
+                {
+
+                }
+                else if (user_input == "2")
+                {
+
+                }
+                else if (user_input == "3")
+                {
+
+                }
+                else if (user_input == "4")
+                {
+
+                }
+                else if (user_input == "5")
+                {
+
+                }
+                else if (user_input == "6")
+                {
+                    running = false;
+                }
+                else
+                {
+                    Console.WriteLine("Error, Please try again.");
+                }
+            }
+        }
 }
 class GoalTracker
 {
@@ -29,5 +74,5 @@ class EternalGoal
 }
 class ChecklistGoal
 {
-    
+
 }
